caf1cac3d5a5ef85676a90110d36e176d013734b
lumoshive-academy <lumoshive.academy@gmail.com>
2025-11-28 15:11:13 +0700

    feat: add subtest in update function student service

--- service/student_test.go
+++ service/student_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"session-9/model"
 	"session-9/repository"
 	"session-9/utils"
@@ -91,30 +92,67 @@ func newTestService() (*StudentService, *repository.MockStudentRepository) {
 	return service, mokeRepo
 }
 
-// func TestStudent_Create(t *testing.T) {
-// 	service, repo := newTestService([]model.Student{})
+func TestStudentService_Create_Success_File_err(t *testing.T) {
+	svc, repo := newTestService()
 
-// 	created, err := service.Create(model.Student{
-// 		Name: "Rudi",
-// 		Age:  20,
-// 	})
+	// existing := []model.Student{
+	// 	{ID: 3, Name: "Andi", Age: 21},
+	// 	{ID: 5, Name: "Siti", Age: 22},
+	// }
 
-// 	if err != nil {
-// 		t.Fatalf("Created returned error: %v", err)
-// 	}
+	repo.On("GetAll").Return([]model.Student{}, utils.ErrFile).Once()
+	// repo.On("SaveAll").Return(nil).Once()
 
-// 	if created.ID != 1 {
-// 		t.Errorf("expected ID 1, got %d", created.ID)
-// 	}
+	input := model.Student{
+		Name: "Budi",
+		Age:  20,
+	}
 
-// 	if created.Name != "Rudi" {
-// 		t.Errorf("expected Name Budi, got %s", created.Name)
-// 	}
+	_, err := svc.Create(input)
 
-// 	if len(repo.Students) != 1 {
-// 		t.Fatalf("expected repo to have 1 student, got %d", len(repo.Students))
-// 	}
-// }
+	assert.Error(t, err)
+	// assert.Equal(t, 6, created.ID)
+	// assert.Equal(t, "Budi", created.Name)
+	assert.Equal(t, utils.ErrFile, err)
+
+	repo.AssertExpectations(t)
+}
+
+func TestStudentService_Create_Error_SaveAll(t *testing.T) {
+	svc, repo := newTestService()
+
+	existing := []model.Student{
+		{ID: 1, Name: "Andi", Age: 21},
+	}
+
+	repo.
+		On("GetAll").
+		Return(existing, nil).
+		Once()
+
+	appendData := []model.Student{
+		{ID: 1, Name: "Andi", Age: 21},
+		{ID: 2, Name: "Budi", Age: 20},
+	}
+
+	repo.
+		On("SaveAll", appendData).
+		Return(errors.New("save error")).
+		Once()
+
+	input := model.Student{
+		Name: "Budi",
+		Age:  20,
+	}
+
+	created, err := svc.Create(input)
+
+	assert.Error(t, err)
+	assert.EqualError(t, err, "save error")
+	assert.Equal(t, model.Student{}, created)
+
+	repo.AssertExpectations(t)
+}
 
 func TestStudentService_GetByID_Found(t *testing.T) {
 	initial := []model.Student{
@@ -140,7 +178,7 @@ func TestStudentService_GetByID_NotFound(t *testing.T) {
 		{ID: 2, Name: "Siti", Age: 22},
 	}
 	svc, repo := newTestService()
-	repo.On("GetAll").Return(initial, utils.ErrFile).Once()
+	repo.On("GetAll").Return(initial, nil).Once()
 
 	_, err := svc.GetByID(999)
 
@@ -173,11 +211,52 @@ func TestStudentService_GetByID_FileError(t *testing.T) {
 	repo.On("GetAll").Return([]model.Student{}, utils.ErrFile).Once()
 
 	_, err := svc.GetByID(1)
-	if err == nil {
-		t.Fatalf("expected error, got nil")
+
+	assert.Error(t, err)
+	assert.Equal(t, utils.ErrFile, err)
+}
+
+func TestStudentService_Update(t *testing.T) {
+	svc, repo := newTestService()
+
+	existing := []model.Student{
+		{ID: 1, Name: "Andi", Age: 21},
+		{ID: 2, Name: "Budi", Age: 20},
 	}
 
-	if err != utils.ErrFile {
-		t.Fatalf("expected error file, got %v", err)
+	input := model.Student{
+		Name: "Budi Dermawan",
+		Age:  20,
 	}
+
+	t.Run("student file error", func(t *testing.T) {
+		repo.
+			On("GetAll").
+			Return([]model.Student{}, utils.ErrFile).
+			Once()
+
+		created, err := svc.Update(2, input)
+
+		assert.Error(t, err)
+		assert.EqualError(t, err, "file error")
+		assert.Equal(t, model.Student{}, created)
+
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("student not found", func(t *testing.T) {
+		repo.
+			On("GetAll").
+			Return(existing, nil).
+			Once()
+
+		created, err := svc.Update(3, input)
+
+		assert.Error(t, err)
+		assert.EqualError(t, err, "student not found")
+		assert.Equal(t, model.Student{}, created)
+
+		repo.AssertExpectations(t)
+	})
+
 }
